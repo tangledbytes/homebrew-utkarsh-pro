b2ed0f9b2a4a3e3244709c9191e4b566d021d5a8
utkarsh-pro <utkarsh@sagacious.dev>
2022-10-07 21:42:29 +0530

    Brew formula update for kindli version v0.2.1

--- Kindli.rb
+++ Kindli.rb
@@ -5,18 +5,18 @@
 class Kindli < Formula
   desc ""
   homepage "https://github.com/utkarsh-pro/kindli"
-  version "0.2.0"
+  version "0.2.1"
 
   depends_on "git"
   depends_on "make"
   depends_on "automake"
-  depends_on "limactl"
+  depends_on "lima"
   depends_on "autoconf"
   depends_on :macos
 
   on_macos do
-    url "https://github.com/utkarsh-pro/kindli/releases/download/v0.2.0/kindli_0.2.0_Darwin_all.tar.gz"
-    sha256 "e5b7844e5b1684a8740bb97f7bf8ae4ddb8eaffdfd1a9172e5d24a381aab0caf"
+    url "https://github.com/utkarsh-pro/kindli/releases/download/v0.2.1/kindli_0.2.1_Darwin_all.tar.gz"
+    sha256 "178105946b269fba4949b8df954fac27cfc8dd6a1296ed2bfa0e3043fb2e68a2"
 
     def install
       bin.install "kindli"
