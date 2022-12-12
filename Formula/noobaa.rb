e8b6b0d724a9314240dbe9fd438f05b4bd1bb99a
NooBaa GitHub Action <github-action@noobaa.io>
2022-12-12 20:03:57 +0000

    noobaa-operator v5.12.16
    Signed-off-by: NooBaa GitHub Action <github-action@noobaa.io>

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -2,8 +2,8 @@ class Noobaa < Formula
   desc "CLI for managing NooBaa S3 service on Kubernetes/Openshift"
   homepage "https://github.com/utkarsh-pro/noobaa-operator-copy"
   url "https://github.com/utkarsh-pro/noobaa-operator-copy.git",
-      :tag      => "v5.12.15",
-      :revision => "9ac9f83a5749a957d9dfc352d4e20ff8da46f8b7"
+      :tag      => "v5.12.16",
+      :revision => "28d10d48fbb06adefc15acd509c838f99a73c74f"
   head "https://github.com/utkarsh-pro/noobaa-operator-copy.git"
 
   depends_on "go" => [:build, :test]
@@ -26,7 +26,7 @@ class Noobaa < Formula
 
   test do
     output = 
-    pos = output.index "CLI version: 5.12.15"
+    pos = output.index "CLI version: 5.12.16"
     raise "Version check failed" if pos.nil?
 
     puts "Success"
