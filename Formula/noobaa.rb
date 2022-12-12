a1fccfb54f767b8dc3dfce489c5021e980fcad61
NooBaa GitHub Action <github-action@noobaa.io>
2022-12-12 17:02:45 +0000

    noobaa-operator v5.12.11
    Signed-off-by: NooBaa GitHub Action <github-action@noobaa.io>

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -3,7 +3,7 @@ class Noobaa < Formula
   homepage "https://github.com/utkarsh-pro/noobaa-operator-copy"
   url "https://github.com/utkarsh-pro/noobaa-operator-copy.git",
       :tag      => "v5.12.11",
-      :revision => "515b054302cf8d7f57a412628ae1138d3fe458be"
+      :revision => "8f63c0874faba0b3ad000ff2a5661b7f9069699f"
   head "https://github.com/utkarsh-pro/noobaa-operator-copy.git"
 
   depends_on "go" => [:build, :test]
